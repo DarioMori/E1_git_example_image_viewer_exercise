e2863820f98dd0c682090fcaa25e49bce7d25800
dario <dario.mori@stud.unifi.it>
2022-04-06 15:50:32 +0200

    commit png

---
+++ E1_git_example_image_viewer_exercise/version.h
@@ -0,0 +1,12 @@
+//
+// Created by dario on 06/04/2022.
+//
+
+#ifndef E1_GIT_EXAMPLE_IMAGE_VIEWER_EXERCISE_VERSION_H
+#define E1_GIT_EXAMPLE_IMAGE_VIEWER_EXERCISE_VERSION_H
+
+#include <string>
+
+std::string const siiiiii
+#endif //E1_GIT_EXAMPLE_IMAGE_VIEWER_EXERCISE_VERSION_H
+
